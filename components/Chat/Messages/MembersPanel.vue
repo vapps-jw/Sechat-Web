3d0497e03f0b915ef66a2526184318e35fdabfc4
JW <jonasz.witek@pm.me>
2023-02-04 23:28:49 +0100

    autoomplete updates

--- components/Chat/Messages/MembersPanel.vue
+++ components/Chat/Messages/MembersPanel.vue
@@ -9,24 +9,27 @@
       <v-btn
         v-bind="props"
         size="small"
-        icon="mdi-cogs"
+        icon="mdi-account-group"
         color="warning"
         variant="outlined"
       ></v-btn>
     </template>
     <v-card>
       <v-toolbar>
-        <v-toolbar-title>{{ props.roomName }} Settings</v-toolbar-title>
+        <v-toolbar-title>{{ props.roomName }} Members</v-toolbar-title>
         <v-spacer></v-spacer>
       </v-toolbar>
       <v-card-text class="ma-0 pa-0 sechat-v-card-text-full">
         <v-autocomplete
+          :custom-filter="hasOccurrences"
           item-title="displayName"
-          :items="chatStore.getApprovedConnections.value"
           item-text="displayName"
+          :items="chatStore.getApprovedConnections.value"
+          item-value="id"
           variant="solo"
           v-model="chosenConnection"
           return-object
+          no-data-text="user not found"
         ></v-autocomplete>
       </v-card-text>
       <v-card-actions>
@@ -50,6 +53,19 @@ interface PropsModel {
   roomName: string;
 }
 const props = defineProps<PropsModel>();
+
+const hasOccurrences = (item: any, queryText: any) => {
+  console.log("--> Lookup item", item);
+  console.log("--> Lookup queryText", queryText);
+  const queryParts = queryText.toLowerCase().split(" ");
+  if (queryParts.length > 0) {
+    return queryParts
+      .map((x) => item.displayName.indexOf(x) > -1)
+      .reduce((a, b) => a && b);
+  }
+
+  return true;
+};
 </script>
 
 <style scoped></style>
