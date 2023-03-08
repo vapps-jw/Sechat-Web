62548a1ed64cd404bfd5a975e36327ac1264e3df
JW <jonasz.witek@pm.me>
2023-03-08 13:07:11 +0100

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,9 @@
         <div class="d-flex justify-center">
           <NuxtImg src="/logos/sechat-black-tr-300x300.png" alt=""></NuxtImg>
         </div>
+        <div class="d-flex justify-center">
+          <v-chip class="ma-2" color="warning"> B E T A </v-chip>
+        </div>
         <v-divider class="my-4"></v-divider>
       </v-container>
       <client-only>
