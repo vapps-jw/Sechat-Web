9656fbc20192f36c85347ce68b9e147fffb5904b
JW <jonasz.witek@pm.me>
2023-03-16 11:56:36 +0100

    refresh refactoring started

---
+++ composables/useRefreshHandler.ts
@@ -0,0 +1,9 @@
+export const useRefreshHandler = () => {
+  const handleVisibilityChange = async () => {};
+
+  const handleChatMounted = async () => {};
+
+  const handleOnlineChange = async () => {};
+
+  return { handleVisibilityChange, handleChatMounted, handleOnlineChange };
+};
