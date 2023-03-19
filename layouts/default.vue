4bf46c1053a13f8848689d6a0b70617f0f78af20
JW <jonasz.witek@pm.me>
2023-03-19 12:05:17 +0100

    default layout updates

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,13 @@
         <div class="d-flex justify-center">
           <NuxtImg src="/logos/sechat-black-tr-300x300.png" alt=""></NuxtImg>
         </div>
-        <div class="d-flex justify-center">
-          <v-chip class="ma-2" color="warning"> B E T A v0.2 </v-chip>
+        <div class="d-flex justify-center flex-wrap">
+          <v-chip class="ma-2" color="warning"> BETA v0.2 </v-chip>
           <v-chip class="ma-2" color="warning">
             For best experience use as PWA
+          </v-chip>
+          <v-chip v-if="!notificationsAllowed" class="ma-2" color="error">
+            Notifications Not Allowed
           </v-chip>
         </div>
         <v-divider class="my-4"></v-divider>
@@ -24,6 +27,10 @@
 if (Notification.permission !== "granted") {
   Notification.requestPermission();
 }
+
+const notificationsAllowed = computed(
+  () => Notification.permission !== "granted"
+);
 </script>
 
 <style scoped>
