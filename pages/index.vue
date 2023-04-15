2435becdf734743091c9a3b2e86214a031644e23
JW <jonasz.witek@pm.me>
2023-04-15 17:47:39 +0200

    about page added

--- pages/index.vue
+++ pages/index.vue
@@ -2,29 +2,35 @@
   <div>
     <v-container>
       <client-only>
-        <div
-          class="d-flex flex-column align-center"
-          v-if="userStore.isSignedIn"
-        >
-          <v-btn class="my-2" size="large">
+        <div class="d-flex flex-column align-center">
+          <v-btn v-if="userStore.isSignedIn" class="my-2" size="large">
             <NuxtLink class="sechat-link-clear" :to="`/chat`">Chat</NuxtLink>
           </v-btn>
-          <v-btn class="my-2" size="large">
+          <v-btn v-if="userStore.isSignedIn" class="my-2" size="large">
             <NuxtLink class="sechat-link-clear" :to="`/user/changePassword`"
               >Change Password</NuxtLink
             >
           </v-btn>
-          <v-btn class="my-2" size="large" @click="signOut">Sign Out</v-btn>
-        </div>
-        <div class="d-flex flex-column align-center" v-else>
-          <v-btn class="my-2" size="large">
+          <v-btn
+            v-if="userStore.isSignedIn"
+            class="my-2"
+            size="large"
+            @click="signOut"
+            >Sign Out</v-btn
+          >
+          <v-btn v-if="!userStore.isSignedIn" class="my-2" size="large">
             <NuxtLink class="sechat-link-clear" :to="`/user/login`"
               >Sign In</NuxtLink
             >
           </v-btn>
-          <v-btn class="my-2" size="large">
+          <v-btn v-if="!userStore.isSignedIn" class="my-2" size="large">
             <NuxtLink class="sechat-link-clear" :to="`/user/register`"
               >Sign Up</NuxtLink
+            >
+          </v-btn>
+          <v-btn class="my-2" size="large">
+            <NuxtLink class="sechat-link-clear" :to="`/details/about`"
+              >FAQ</NuxtLink
             >
           </v-btn>
         </div>
