40cf875d6825a3ae590a7858786bf063056d1486
JW <jonasz.witek@pm.me>
2023-04-25 09:56:45 +0200

    update and contacts update

---
+++ components/Chat/Contacts/List.vue
@@ -0,0 +1,150 @@
+<template>
+  <v-expansion-panels class="mt-2">
+    <v-expansion-panel
+      v-for="uc in chatStore.getConnections"
+      :key="`${uc.invitedName}-${uc.inviterName}`"
+    >
+      <v-expansion-panel-title disable-icon-rotate>
+        {{ uc.displayName }}
+        <template v-slot:actions>
+          <v-icon v-if="uc.blocked" color="error">mdi-alert-circle</v-icon>
+          <v-icon v-else-if="uc.approved" color="success"
+            >mdi-check-bold</v-icon
+          >
+          <v-icon v-else color="warning">mdi-help-circle-outline</v-icon>
+        </template>
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-row no-gutters>
+          <v-btn
+            v-if="uc.blocked && uc.blockedByName === userStore.getUserName"
+            @click="async () => allowConnection(uc.id)"
+            class="mx-1"
+            size="small"
+            icon="mdi-checkbox-marked-circle-outline"
+            color="success"
+            variant="outlined"
+          ></v-btn>
+          <v-btn
+            v-else
+            @click="async () => blockConnection(uc.id)"
+            class="mx-1"
+            size="small"
+            icon="mdi-block-helper"
+            color="error"
+            variant="outlined"
+          ></v-btn>
+          <v-btn
+            v-if="!uc.approved && uc.invitedName === userStore.getUserName"
+            @click="async () => approveConnection(uc.id)"
+            class="mx-1"
+            size="small"
+            icon="mdi-check-bold"
+            color="success"
+            variant="outlined"
+          ></v-btn>
+          <v-btn
+            @click="async () => deleteConnection(uc.id)"
+            class="mx-1"
+            size="small"
+            icon="mdi-delete"
+            color="error"
+            variant="outlined"
+          ></v-btn>
+          <v-spacer />
+        </v-row>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script setup lang="ts">
+import { SnackbarMessages } from "~~/utilities/globalEnums";
+
+const chatStore = useSechatChatStore();
+const config = useRuntimeConfig();
+const sechatApp = useSechatApp();
+const userStore = useUserStore();
+
+const blockConnection = async (id: number) => {
+  console.log("--> Calling connection block");
+
+  const { error: apiError } = await useFetch(
+    `${config.public.apiBase}/user/block-connection/?connectionId=${id}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+      credentials: "include",
+    }
+  );
+
+  if (apiError.value) {
+    sechatApp.showErrorSnackbar(apiError.value.data);
+    return;
+  }
+  sechatApp.showSuccessSnackbar("Contact blocked");
+};
+
+const allowConnection = async (id: number) => {
+  console.log("--> Calling connection block");
+  const { error: apiError } = await useFetch(
+    `${config.public.apiBase}/user/allow-connection/?connectionId=${id}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+      credentials: "include",
+    }
+  );
+
+  if (apiError.value) {
+    sechatApp.showErrorSnackbar(SnackbarMessages.Error);
+    return;
+  }
+  sechatApp.showSuccessSnackbar(SnackbarMessages.Success);
+};
+
+const approveConnection = async (id: number) => {
+  console.log("--> Calling connection approve");
+  const { error: apiError } = await useFetch(
+    `${config.public.apiBase}/user/approve-connection/?connectionId=${id}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "PATCH",
+      credentials: "include",
+    }
+  );
+
+  if (apiError.value) {
+    sechatApp.showErrorSnackbar(apiError.value.data);
+    return;
+  }
+
+  sechatApp.showSuccessSnackbar("Contact approved");
+};
+
+const deleteConnection = async (id: number) => {
+  console.log("--> Deleting connection", id);
+  const { error: apiError } = await useFetch(
+    `${config.public.apiBase}/user/delete-connection/?connectionId=${id}`,
+    {
+      method: "DELETE",
+      credentials: "include",
+    }
+  );
+
+  if (apiError.value) {
+    sechatApp.showErrorSnackbar(apiError.value.data);
+    return;
+  }
+
+  sechatApp.showSuccessSnackbar("Contact deleted");
+};
+</script>
+
+<style scoped></style>
