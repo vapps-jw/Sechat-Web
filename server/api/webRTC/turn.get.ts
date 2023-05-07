d722cac0be9e1fbbf64108a850986c22a5421813
JW <jonasz.witek@pm.me>
2023-05-07 21:59:05 +0200

    servers update

--- server/api/webRTC/turn.get.ts
+++ server/api/webRTC/turn.get.ts
@@ -1,28 +1,8 @@
 export default defineEventHandler(async (e) => {
   const config = useRuntimeConfig();
-
-  // console.log(`TURN User: ${config.turnApiKey}`);
-  // if (!config.turnApiKey) {
-  //   throw createError({
-  //     statusCode: 400,
-  //     statusMessage: "No API key",
-  //   });
-  // }
-
-  // const response: any = await $fetch(
-  //   `https://yourappname.metered.live/api/v1/turn/credentials?apiKey=${config.turnApiKey}`
-  // );
-
-  const googleStun = [
+  const servers = [
     {
       urls: config.stunServer,
-      // urls: [
-      //   "stun:stun.l.google.com:19302",
-      //   "stun:stun1.l.google.com:19302",
-      //   "stun:stun2.l.google.com:19302",
-      //   "stun:stun3.l.google.com:19302",
-      //   "stun:stun4.l.google.com:19302",
-      // ],
     },
     {
       urls: config.turnServer,
@@ -30,6 +10,5 @@ export default defineEventHandler(async (e) => {
       credential: config.turnPassword,
     },
   ];
-  //response[0] = { urls: [...googleStun.urls, response[0].urls] };
-  return googleStun;
+  return servers;
 });
