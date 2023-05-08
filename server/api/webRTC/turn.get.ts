0ce7c8b8dd671c760451d7d6bcccb52d2767420f
JW <jonasz.witek@pm.me>
2023-05-08 17:46:02 +0200

    update

--- server/api/webRTC/turn.get.ts
+++ server/api/webRTC/turn.get.ts
@@ -2,16 +2,10 @@ export default defineEventHandler(async (e) => {
   const config = useRuntimeConfig();
   const servers = [
     {
-      urls: [
-        "stun:stun.l.google.com:19302",
-        "stun:stun1.l.google.com:19302",
-        "stun:stun2.l.google.com:19302",
-        "stun:stun3.l.google.com:19302",
-        "stun:stun4.l.google.com:19302",
-      ],
+      urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"],
     },
     {
-      urls: config.turnServer,
+      urls: [config.turnServer],
       username: config.turnUser,
       credential: config.turnPassword,
     },
