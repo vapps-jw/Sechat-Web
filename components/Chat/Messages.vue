33a90a19dc472e9d3b8f6242afefdad9265bf25f
JW <jonasz.witek@pm.me>
2023-05-27 21:32:22 +0200

    preparations

--- components/Chat/Messages.vue
+++ components/Chat/Messages.vue
@@ -15,38 +15,7 @@
       </v-toolbar>
       <chat-messages-room-members />
       <v-divider />
-      <v-sheet id="chatView" class="ma-0 pa-0 overflow-auto">
-        <div
-          class="d-flex"
-          :class="
-            isActiveUser(message)
-              ? 'flex-row-reverse ma-1 '
-              : 'flex-row flex-start ma-1 '
-          "
-          v-for="message in chatStore.getActiveRoom.messages"
-        >
-          <chat-messages-message :message="message" />
-        </div>
-
-        <!-- <v-virtual-scroll
-          ref="messagesVirtualScrollRef"
-          scrollToBottom="true"
-          :items="chatStore.getActiveRoom.messages"
-        >
-          <template v-slot:default="{ item }">
-            <div
-              class="d-flex"
-              :class="
-                isActiveUser(item)
-                  ? 'flex-row-reverse ma-1 '
-                  : 'flex-row flex-start ma-1 '
-              "
-            >
-              <chat-messages-message :message="item" />
-            </div>
-          </template>
-        </v-virtual-scroll> -->
-      </v-sheet>
+      <chat-messages-container />
       <v-spacer />
       <chat-messages-text-editor />
     </v-card>
@@ -54,28 +23,5 @@
 </template>
 
 <script setup lang="ts">
-import { scrollToBottom } from "@/utilities/documentFunctions";
-
 const chatStore = useSechatChatStore();
-const userStore = useUserStore();
-
-// const messagesVirtualScrollRef = ref()
-
-// const scroll = () => {
-//   messagesVirtualScrollRef.value?.scrollToIndex(500)
-// }
-
-onUpdated(() => {
-  console.warn("--> onUpdated Scrolling");
-  scrollToBottom("chatView");
-});
-
-onMounted(() => {
-  console.warn("--> onMounted Scrolling");
-  scrollToBottom("chatView");
-});
-
-const isActiveUser = (message: IMessage) => {
-  return message.nameSentBy === userStore.getUserName;
-};
 </script>
---
+++ components/Chat/Messages/Container.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-sheet id="chatView" class="ma-0 pa-0 overflow-auto">
+    <div
+      class="d-flex"
+      :class="
+        isActiveUser(message)
+          ? 'flex-row-reverse ma-1 '
+          : 'flex-row flex-start ma-1 '
+      "
+      v-for="message in chatStore.getActiveRoom.messages"
+    >
+      <chat-messages-message :message="message" />
+    </div>
+
+    <!-- <v-virtual-scroll
+          ref="messagesVirtualScrollRef"
+          scrollToBottom="true"
+          :items="chatStore.getActiveRoom.messages"
+        >
+          <template v-slot:default="{ item }">
+            <div
+              class="d-flex"
+              :class="
+                isActiveUser(item)
+                  ? 'flex-row-reverse ma-1 '
+                  : 'flex-row flex-start ma-1 '
+              "
+            >
+              <chat-messages-message :message="item" />
+            </div>
+          </template>
+        </v-virtual-scroll> -->
+  </v-sheet>
+</template>
+
+<script setup lang="ts">
+import { scrollToBottom } from "@/utilities/documentFunctions";
+
+const chatStore = useSechatChatStore();
+const userStore = useUserStore();
+
+// const messagesVirtualScrollRef = ref()
+
+// const scroll = () => {
+//   messagesVirtualScrollRef.value?.scrollToIndex(500)
+// }
+
+onUpdated(() => {
+  console.warn("--> onUpdated Scrolling");
+  scrollToBottom("chatView");
+});
+
+onMounted(() => {
+  console.warn("--> onMounted Scrolling");
+  scrollToBottom("chatView");
+});
+
+const isActiveUser = (message: IMessage) => {
+  return message.nameSentBy === userStore.getUserName;
+};
+</script>
+
+<style scoped></style>
