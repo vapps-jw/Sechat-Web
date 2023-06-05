134a9fb7223608c1c64406a32fb41873dbad2ca8
JW <jonasz.witek@pm.me>
2023-06-05 16:07:00 +0200

    updates

--- pages/chat/share.vue
+++ pages/chat/share.vue
@@ -4,7 +4,11 @@
       <v-toolbar>
         <v-toolbar-title>Share</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn icon="mdi-share" variant="outlined"></v-btn>
+        <v-btn
+          icon="mdi-share"
+          variant="outlined"
+          @click="executeShare"
+        ></v-btn>
       </v-toolbar>
       <v-card-text class="ma-0 pa-0 overflow-auto">
         <v-container>
@@ -26,16 +30,22 @@ definePageMeta({
   middleware: ["authenticated"],
 });
 
+const shareData = ref<IShareDetails>(null);
 const shareTargetUpdate = (roomId: string) => {
   console.log("--> ShareTarget", roomId);
-  const shareData = {
+  shareData.value = {
     roomId: roomId,
     title: title.value,
     text: text.value,
   } as IShareDetails;
+};
 
-  chatStore.selectRoom(shareData.roomId);
-  chatStore.newMessage = `${shareData.title} ${shareData.text} `;
+const executeShare = () => {
+  if (!shareData.value) {
+    return;
+  }
+  chatStore.selectRoom(shareData.value.roomId);
+  chatStore.newMessage = `${shareData.value.title} ${shareData.value.text} `;
   navigateTo("/chat");
 };
 </script>
