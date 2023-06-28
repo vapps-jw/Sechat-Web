2392fb73771bad55b1dbc7a9dba2162b8db49f46
JW <jonasz.witek@pm.me>
2023-06-28 23:16:29 +0200

    logo

--- components/Chat/Messages.vue
+++ components/Chat/Messages.vue
@@ -1,10 +1,15 @@
 <template>
-  <v-container v-if="!chatStore.getActiveRoom">
-    <v-card class="mx-auto sechat-v-card-full">
-      <div class="px-4 py-2 text-center w-100">
-        <strong>Select Room</strong>
-      </div>
-    </v-card>
+  <v-container
+    v-if="!chatStore.getActiveRoom"
+    class="bg-transparent d-flex justify-center align-center flex-column sechat-v-card-full"
+  >
+    <NuxtImg
+      src="/logos/logo-only-transparent-300x300.png"
+      alt=""
+      width="300"
+      height="150"
+    ></NuxtImg>
+    <div class="text-h6">Select a Room or createa new one</div>
   </v-container>
   <v-container v-else>
     <v-card class="sechat-v-card-full">
