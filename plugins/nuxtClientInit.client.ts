f75f31313abf27256fb0273a01914476e11a1473
JW <jonasz.witek@pm.me>
2023-07-06 17:25:11 +0200

    console dropped

--- plugins/nuxtClientInit.client.ts
+++ plugins/nuxtClientInit.client.ts
@@ -1,5 +1,14 @@
 export default defineNuxtPlugin(async (context) => {
+  const disableLogs = () => {
+    console.log = () => {};
+    console.warn = () => {};
+    console.info = () => {};
+  };
+
   console.warn("--> Client Init Plugin Triggered");
+  console.warn("Disabling console", process.env.NODE_ENV);
+  process.env.NODE_ENV === "production" ? disableLogs() : null;
+
   const userData = useUserApi();
 
   await userData.getUserData();
