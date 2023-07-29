a5f5bc4934eb428a7eb06c5142383bf8694e9447
JW <jonasz.witek@pm.me>
2023-07-29 17:29:27 +0200

    ping removed

--- composables/useSechatApp.ts
+++ composables/useSechatApp.ts
@@ -2,20 +2,6 @@ import { SnackbarIcons } from "~~/utilities/globalEnums";
 import { PushNotificationTypes } from "~~/utilities/globalEnums";
 
 export const useSechatApp = () => {
-  const config = useRuntimeConfig();
-  const useAppStore = useSechatAppStore();
-
-  const startPing = () => {
-    useAppStore.pingServerInterval = setInterval(
-      async () => await pingServer(),
-      2000
-    );
-  };
-
-  const stopPing = () => {
-    clearInterval(useAppStore.pingServerInterval);
-  };
-
   const clearNotifications = async () => {
     if ("serviceWorker" in navigator) {
       console.warn("--> Clearing notifications...");
@@ -43,24 +29,8 @@ export const useSechatApp = () => {
     }
   };
 
-  const pingServer = async () => {
-    console.log("--> Ping Server");
-    await useFetch(`${config.public.apiBase}/status/ping-api`, {
-      method: "GET",
-      onResponse({ response }) {
-        console.log("--> Ping Result", response.status);
-      },
-      onResponseError({ response }) {
-        console.log("--> Ping Result", response.status);
-      },
-    });
-  };
-
   return {
     clearVideoCallNotifications,
     clearNotifications,
-    pingServer,
-    startPing,
-    stopPing,
   };
 };
